c1cac078782d372f7f29e077897eaa8541304d1c
Tonny-Oliver <ctt.tonny@gmail.com>
2024-02-29 01:55:36 -0300

    Atualizado

--- virt/Scripts/activate.bat
+++ virt/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Tonny\Desktop\Filmoteca\virt
+set VIRTUAL_ENV=C:\Users\Tonny\Desktop\Filmoteca\Virt
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -16,7 +16,7 @@ if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
 set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(virt) %PROMPT%
+set PROMPT=(Virt) %PROMPT%
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
@@ -25,7 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(virt) 
+set VIRTUAL_ENV_PROMPT=(Virt) 
 
 :END
 if defined _OLD_CODEPAGE (
